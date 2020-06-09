292b136759682b6e77c74e46a1e5220348bedf75
Czechbol <adamludes@gmail.com>
2020-06-09 20:25:55 +0200

    Using $(...) instead of legacy `...`

--- resources/backup.docker.sh
+++ resources/backup.docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p ~/backups
-docker exec -it amadeus_db_1 pg_dumpall -c -U postgres > ~/backups/dump_`date +%Y-%m-%d"_"%H:%M:%S`.sql
+docker exec -it amadeus_db_1 pg_dumpall -c -U postgres > ~/backups/dump_$(date +%Y-%m-%d"_"%H:%M:%S).sql
 exit 0
