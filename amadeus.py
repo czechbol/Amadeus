48abe6fe02a37063cf948e7cf87a167bac280e41
Czechbol <adamludes@gmail.com>
2020-08-19 00:10:31 +0200

    unverify db

--- amadeus.py
+++ amadeus.py
@@ -13,6 +13,7 @@ from repository.database import database
 from repository.database.vote import Vote  # noqa F401
 from repository.database.remind import Reminder  # noqa F401
 from repository.database.user_channels import UserChannel  # noqa F401
+from repository.database.unverify import Unverify  # noqa F401
 
 bot = commands.Bot(
     command_prefix=commands.when_mentioned_or(*config.prefixes),
---
+++ repository/database/unverify.py
@@ -0,0 +1,17 @@
+from sqlalchemy import ARRAY, Column, Integer, String, DateTime, BigInteger
+from repository.database import database
+
+
+class Unverify(database.base):
+    __tablename__ = "unverify_table"
+
+    idx = Column(Integer, primary_key=True, autoincrement=True)
+    guild_id = Column(BigInteger)
+    user_id = Column(BigInteger)
+    start_time = Column(DateTime)
+    end_time = Column(DateTime)
+    roles_to_return = Column(ARRAY(Integer))
+    channels_to_return = Column(ARRAY(Integer))
+    channels_to_remove = Column(ARRAY(Integer))
+    reason = Column(String)
+    status = Column(String, default="waiting")
