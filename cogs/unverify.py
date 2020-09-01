eea4e0eff6a30c53843fc562a70deaa0662f55b0
Czechbol <adamludes@gmail.com>
2020-09-01 23:54:47 +0200

    working self unverify

--- cogs/unverify.py
+++ cogs/unverify.py
@@ -5,6 +5,7 @@ from datetime import timedelta
 from dateparser.search import search_dates
 
 import discord
+from discord import CategoryChannel
 from discord.ext import tasks, commands
 
 from core import basecog
@@ -31,19 +32,19 @@ class Unverify(basecog.Basecog):
         repo = repository.get_waiting()
         if repo != []:
             for row in repo:
-                duration = row.new_date - datetime.now()
+                duration = row.end_time - datetime.now()
                 duration_in_s = duration.total_seconds()
-                if row.new_date < datetime.now():
+                if row.end_time < datetime.now():
                     await self.reverify_user(row)
                 elif duration_in_s < 10:
                     await self.reverify_user(row, time=duration_in_s)
         repo = repository.get_finished()
         if repo != []:
             for row in repo:
-                if row.new_date < (datetime.now() - timedelta(days=7)):
+                if row.end_time < (datetime.now() - timedelta(days=7)):
                     await self.log(
                         level="debug",
-                        message=f"Deleting unverify from db: ID: {row.idx}, time: {row.new_date}, status: {row.status}, \nmessage: {row.message}",
+                        message=f"Deleting unverify from db: ID: {row.idx}, time: {row.end_time}, status: {row.status}, \nmessage: {row.reason}",
                     )
                     repository.delete(row.idx)
 
@@ -84,40 +85,131 @@ class Unverify(basecog.Basecog):
 
         return date, date_str
 
-    async def get_embed(self, row, unverified_by):
-        user = self.bot.get_user(row.user_id)
-        if user is None:
-            try:
-                user = await self.bot.fetch_user(row.user_id)
-            except discord.errors.NotFound:
-                return
-        if unverified_by != user:
-            title = text.get("unverify", "unverified by")
-        else:
-            title = "Self unverify"
-
-        reverify_on = row.end_time.strftime("%d.%m.%Y %H:%M")
-
-        embed = self.create_embed(author=user, title=title, description=text.get("unverify", "unverified"))
-        embed.add_field(name=text.get("unverify", "reverify on"), value=reverify_on, inline=True)
-        if row.reason != "Self unverify":
-            embed.add_field(name=text.get("unverify", "reason title"), value=row.reason, inline=False)
-
-        return embed, user
-
     async def reverify_user(self, row, time=None):
-        user = self.bot.get_user(row.user_id)
-        if user is None:
-            try:
-                user = await self.bot.fetch_user(row.user_id)
-            except discord.errors.NotFound:
-                return
+        guild = self.bot.get_guild(row.guild_id)
+        member = guild.get_member(row.user_id)
+        if member is None:
+            return
 
         if time is not None:
             await asyncio.sleep(time)
-        await self.log(level="info", message=f"Reverifying {user.name}")
-        # TODO await user.send(embed=embed)
+        await self.log(level="info", message=f"Reverifying {member.name}")
+        roles = []
+        for role_id in row.roles_to_return:
+            role = discord.utils.get(guild.roles, id=role_id)
+            roles.append(role)
+        try:
+            await member.add_roles(*roles, reason=None, atomic=True)
+        except discord.errors.Forbidden:
+            pass
+        for channel_id in row.channels_to_return:
+            channel = discord.utils.get(guild.channels, id=channel_id)
+            user_overw = channel.overwrites_for(member)
+            user_overw.update(read_messages=True)
+            await channel.set_permissions(member, overwrite=user_overw, reason="Unverify")
+
+        for channel_id in row.channels_to_remove:
+            channel = discord.utils.get(guild.channels, id=channel_id)
+            user_overw = channel.overwrites_for(member)
+            user_overw.update(read_messages=False)
+            await channel.set_permissions(member, overwrite=user_overw, reason="Unverify")
+
+        for id in config.roles_unverify:
+            role = discord.utils.get(guild.roles, id=id)
+            if role is not None:
+                unverify_role = role
+                try:
+                    await member.remove_roles(unverify_role, reason="Reverify", atomic=True)
+                except discord.errors.Forbidden:
+                    pass
+                break
+            else:
+                return None
+
+        await member.send(f"Byly ti vráceny práva na serveru {guild.name}.")
         repository.set_finished(row.idx)
+
+    async def unverify_user(
+        self,
+        ctx: commands.Context,
+        member: discord.abc.User,
+        lines: str,
+        date: datetime,
+        func: str,
+        args: str = "",
+    ):
+        roles_to_keep = []
+        roles_to_remove = []
+        channels_to_keep = []
+        removed_channels = []
+
+        for id in config.roles_unverify:
+            role = discord.utils.get(ctx.guild.roles, id=id)
+            if role is not None:
+                unverify_role = role
+                break
+            else:
+                return None
+
+        for value in args:
+            role = discord.utils.get(ctx.guild.roles, name=value)
+            channel = discord.utils.get(ctx.guild.channels, name=value)
+            if role is not None and role in member.roles:
+                roles_to_keep.append(role)
+            elif channel is not None:
+                perms = channel.permissions_for(member)
+
+                if not perms.read_messages:
+                    continue
+                channels_to_keep.append(channel)
+
+        for role in member.roles:
+            if role not in roles_to_keep and role.position != 0:
+                roles_to_remove.append(role)
+        try:
+            await member.remove_roles(*roles_to_remove, reason=func, atomic=True)
+        except discord.errors.Forbidden:
+            pass
+        await member.add_roles(unverify_role, reason=func, atomic=True)
+        await asyncio.sleep(2)
+
+        for channel in member.guild.channels:
+            if not isinstance(channel, CategoryChannel):
+                perms = channel.permissions_for(member)
+                user_overw = channel.overwrites_for(member)
+
+                if channel in channels_to_keep:
+                    if not perms.read_messages:
+                        user_overw.update(read_messages=True)
+                        await channel.set_permissions(member, overwrite=user_overw, reason=func)
+                elif perms.read_messages and not user_overw.read_messages:
+                    pass
+                elif not perms.read_messages:
+                    pass
+                else:
+                    user_overw.update(read_messages=False)
+                    await channel.set_permissions(member, overwrite=user_overw, reason=func)
+                    removed_channels.append(channel.id)
+
+        removed_roles = [role.id for role in roles_to_remove]
+        added_channels = [channel.id for channel in channels_to_keep]
+
+        lines = "\n".join(lines)
+        if len(lines) > 1024:
+            lines = lines[:1024]
+            lines = lines[:-3] + "```" if lines.count("```") % 2 != 0 else lines
+
+        result = repository.add(
+            guild_id=member.guild.id,
+            user_id=member.id,
+            start_time=datetime.now(),
+            end_time=date,
+            roles_to_return=removed_roles,
+            channels_to_return=removed_channels,
+            channels_to_remove=added_channels,
+            reason=lines,
+        )
+        return result
 
     @commands.cooldown(rate=5, per=20.0, type=commands.BucketType.user)
     @commands.command(
@@ -131,6 +223,9 @@ class Unverify(basecog.Basecog):
         arg = lines.pop(0)
         arg = arg.replace("weekend", "saturday")
         date, date_str = await self.parse_datetime(arg)
+        printdate = date.strftime("%d.%m.%Y %H:%M:%S")
+        member = ctx.message.author
+        await self.log(level="info", message=f"Selfunverify: Member - {member.name}, Until - {date}")
 
         if date is None:
             if len(lines) == 0:
@@ -147,35 +242,19 @@ class Unverify(basecog.Basecog):
         args = re.split("[;, \n]", arg)
         while "" in args:
             args.remove("")
-        roles_to_keep = []
 
-        for value in args:
-            role = discord.utils.get(ctx.guild.roles, name=value)
-            channel = discord.utils.get(ctx.guild.channels, name=value)
-            if role is not None and role in ctx.message.author.roles:
-                roles_to_keep.append(role)
-            elif channel is not None:
-                perms = channel.permissions_for(ctx.message.author)
-                user_overw = channel.overwrites_for(ctx.message.author)
+        result = await self.unverify_user(
+            ctx, member=member, args=args, lines=lines, date=date, func="Self unverify"
+        )
 
-                if not perms.read_messages:
-                    continue
+        if result is not None:
+            await self.log(
+                level="debug", message=f"Selfunverify success: Member - {member.name}, Until - {date}"
+            )
 
-                for role in ctx.message.author.roles:
-                    overwrites = channel.overwrites_for(role)
-                    if overwrites.read_messages and user_overw.read_messages is None:
-                        user_overw.update(read_messages=True)
-                        await channel.set_permissions(
-                            ctx.message.author, overwrite=user_overw, reason="Unverify"
-                        )
-                        break
-
-        lines = "\n".join(lines)
-        if len(lines) > 1024:
-            lines = lines[:1024]
-            lines = lines[:-3] + "```" if lines.count("```") % 2 != 0 else lines
-
-        return
+        await member.send(
+            f"Byly ti dočasně odebrány všechny práva na serveru {ctx.guild.name}. Přístup ti bude navrácen {printdate}. Důvod: Self unverify"
+        )
 
 
 def setup(bot):
