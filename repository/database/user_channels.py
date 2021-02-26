efbf9f6c1898a706561454901536ae3bd0f97516
czechbol <adamludes@gmail.com>
2021-02-26 16:50:04 +0100

    user_channel table tweak

--- repository/database/user_channels.py
+++ repository/database/user_channels.py
@@ -5,10 +5,18 @@ from repository.database import database
 class UserChannel(database.base):
     __tablename__ = "user_channels"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    idx = Column(Integer, primary_key=True, autoincrement=True)
     guild_id = Column(BigInteger)
     channel_id = Column(BigInteger)
     user_id = Column(BigInteger)
     is_webhook = Column(Boolean)
     count = Column(BigInteger, default=1)
     last_msg_at = Column(DateTime)
+
+    def __repr__(self):
+        return (
+            f'<UserChannel idx="{self.idx}" '
+            f'guild_id="{self.guild_id}" channel_id="{self.channel_id}" '
+            f'user_id="{self.user_id}" is_webhook="{self.is_webhook}"'
+            f'count="{self.user_id}" last_msg_at="{self.is_webhook}">'
+        )
